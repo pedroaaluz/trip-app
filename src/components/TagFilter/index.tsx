9a1659d05cff39a6e5eab97e93b4af330d8746d2
pedroaaluz <pedroaluz137@gmail.com>
2023-04-04 17:50:51 -0300

    feat: :sparkles: add interface in tagFilter component

--- src/components/TagFilter/index.tsx
+++ src/components/TagFilter/index.tsx
@@ -1,23 +1,28 @@
-import React /* , {useState} */ from 'react';
-import {Box, Icon, Text} from 'native-base';
+import React from 'react';
+import {Box, Icon, Text, Switch} from 'native-base';
 import Feather from 'react-native-vector-icons/Feather';
 
-const TagFilter = ({name, icon}): JSX.Element => {
+interface TagFilterProps {
+  name: string;
+  icon: string;
+}
+
+const TagFilter = ({name, icon}: TagFilterProps): JSX.Element => {
   return (
     <Box
-      alignItems={'center'}
-      flexDirection={'column'}
-      padding={6}
-      borderColor={'#b8b0b0'}
-      borderWidth={1}
       marginX={2}
+      marginBottom={5}
+      justifyContent={'space-around'}
       minWidth={40}
-      borderRadius={20}
+      flexDirection={'row'}
       flex={1}>
-      <Icon as={Feather} size={8} name={icon} color={'#b8b0b0'} />
-      <Text fontSize="lg" marginTop={2}>
-        {name}
-      </Text>
+      <Box flex={1} flexDirection={'row'} alignItems={'center'}>
+        <Icon as={Feather} size={5} name={icon} color={'#b8b0b0'} />
+        <Text marginLeft={3} fontSize="lg">
+          {name}
+        </Text>
+      </Box>
+      <Switch size="md" />
     </Box>
   );
 };
