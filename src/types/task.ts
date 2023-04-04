fbc7db37a385c8792a0d8012b7dc7118a7491084
pedroaaluz <pedroaluz137@gmail.com>
2023-04-04 16:42:02 -0300

    refactor: :fire: remove uncessary files

--- src/types/task.ts
+++
@@ -1,6 +0,0 @@
-export type TaskInterface = {
-  name: string;
-  description: string;
-  date: string;
-  id: number;
-};
--- src/utils/formatDate.ts
+++
@@ -1,6 +0,0 @@
-import {format} from 'date-fns';
-
-export const formatDate = (date: Date) => {
-  const formatString = 'MM/dd/yyyy, HH:mm:ss';
-  return format(date, formatString);
-};
